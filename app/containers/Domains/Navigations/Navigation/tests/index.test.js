e9ffa219cad534c8a2f31dcf78866c5612ff4695
Ehren Davis <ehren.davis12@gmail.com>
2019-06-03 21:26:59 -0700

    Started the Unit testing of the viewMessages container

--- app/containers/Domains/Navigations/Navigation/tests/index.test.js
+++ app/containers/Domains/Navigations/Navigation/tests/index.test.js
@@ -1,11 +1,38 @@
-// import React from 'react';
-// import { mount } from 'enzyme';
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 // import { enzymeFind } from 'styled-components/test-utils';
+import { browserHistory } from 'react-router-dom';
+import configureStore from '../../../../../configureStore';
 
-// import { Navigation } from '../index';
+import Navigation from '../index';
 
 describe('<Navigation />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  let store;
+  let wrapper;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+    wrapper = renderComponentMount();
+  });
+
+  const renderComponentMount = () =>
+    mount(
+      <Provider store={store}>
+        <Navigation />
+      </Provider>,
+    );
+  // mount(<Navigation {...navigationLinksProps} />);
+
+  describe('<Navigation /> snap Shots', () => {
+    it('renders as expected with populated messages', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('<Navigation />', () => {
+    it('should render its NavigationBody', () => {
+      expect(wrapper.find('NavigationBody').length).toEqual(1);
+    });
   });
 });
