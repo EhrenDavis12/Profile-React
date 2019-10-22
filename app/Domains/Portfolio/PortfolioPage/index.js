ab3e199f8e4f65a29368f2b9e29f9bc7b89e65d8
Ehren Davis <ehren.davis12@gmail.com>
2019-10-21 20:05:11 -0700

    stage a test

--- app/Domains/Portfolio/PortfolioPage/index.js
+++ app/Domains/Portfolio/PortfolioPage/index.js
@@ -6,7 +6,7 @@
  * NOTE: while this component should technically be a stateless functional
  * component (SFC), hot reloading does not currently support SFCs. If hot
  * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
+ * the linting exception
  */
 
 import React from 'react';
