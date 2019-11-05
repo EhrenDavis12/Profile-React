5560c3bd524483d4718b8623d87d0234acefc49b
Ehren Davis <ehren.davis12@gmail.com>
2019-11-04 20:18:53 -0700

    Fix Parallax Banner

---
+++ app/components/Banners/tests/ParallaxBanner.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import 'jest-styled-components';
+import BkgImg6 from 'images/small_bluePur_sky.jpg';
+import ParallaxBanner from '../ParallaxBanner';
+
+describe('<ParallaxBanner />', () => {
+  it('should render the page default Strength', () => {
+    const renderedComponent = shallow(
+      <ParallaxBanner image={BkgImg6} id="Hobbies" header="Hobbies">
+        <div>Hello World</div>
+      </ParallaxBanner>,
+    );
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it('should render the page given Strength', () => {
+    const renderedComponent = shallow(
+      <ParallaxBanner
+        image={BkgImg6}
+        id="Hobbies"
+        header="Hobbies"
+        strength={1000}
+      >
+        <div>Hello World</div>
+      </ParallaxBanner>,
+    );
+    expect(renderedComponent).toMatchSnapshot();
+  });
+});
